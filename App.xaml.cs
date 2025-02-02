b7c072adc32a4268bcaf21a99ccdd5aca1acc871
RMix2024 <rob.mix.1981@gmail.com>
2025-02-01 18:32:14 -0600

    Transactions page looing good

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,12 +1,19 @@
-﻿namespace MoneyMap
+﻿
+
+namespace MoneyMap
 {
     public partial class App : Application
     {
         public App()
         {
             InitializeComponent();
-        }
 
+         // Apply Windows-specific style globally
+#if WINDOWS
+            Resources["DefaultPageStyle"] = Resources["WindowsPageStyle"];
+#endif
+        
+        }
         protected override Window CreateWindow(IActivationState? activationState)
         {
             return new Window(new AppShell());
